d8ba2116c3c1043e3df692f54d9ecd9cf9d5f0f6
Felipe Lima Coelho <felipeapoloatena@gmail.com>
2023-05-10 22:58:38 -0300

    Criado a base do componente App juntamente com um componente Loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Loading from './components/Loading';
 
 class App extends Component {
-  render() {
-    return (
-      <div>App</div>
-    )
-  }
-}
+  state = {
+    isLoading: false,
+    fetched: false,
+  };
 
-export default App
+  render() {
+    const { isLoading, fetched } = this.state;
+
+    if (isLoading) return <Loading />;
+    return (
+      <>
+        <main>
+          <form>
+            <label htmlFor="input-search">
+              Search Game Of Thrones character:
+              <input
+                type="text"
+                name="input-search"
+                id="input-search"
+                placeholder="Ex.: Tyrion Lannister"
+              />
+            </label>
+            <button>Search</button>
+          </form>
+
+          {
+            fetched && (
+              <section>
+                <p><strong>Name:</strong></p>
+                <p><strong>Born:</strong></p>
+                <p><strong>Titles:</strong></p>
+                <p><strong>Aliases:</strong></p>
+                <p><strong>Tv Series:</strong></p>
+                <p><strong>Played By:</strong></p>
+              </section>
+            )
+          }
+        </main>
+      </>
+    );
+  }
+};
+
+export default App;
